c21da187d064292be4b83f2fab6aeab5f007e7e1
HippoProgrammer <172101796+HippoProgrammer@users.noreply.github.com>
2026-06-09 13:12:11 +0000

    Add placeholder parsing function

--- src/customio/ns.py
+++ src/customio/ns.py
@@ -117,12 +117,26 @@ class API:
         try:
             xmlstr = await self._make_request(f'http://www.nationstates.net/cgi-bin/api.cgi?wa={council}&q=resolution')
             xmltree = etree.fromstring(xmlstr)
-            resolutions = xmltree.findall('./RESOLUTION')
-            if len(resolutions) == 0:
+            resolution_elements = xmltree.findall('./RESOLUTION/')
+            if len(resolution_elements) == 0:
                 return None
             else:
-                return resolutions
+                return resolution_elements
         except RateLimitedException as e:
             logger.error(str(e))
         except HTTPResponseException as e:
             raise QueryException(str(e)) 
+    
+    async def parse_atvote(self, council:int):
+        xml = await self._query_atvote(council)
+        parsed_xml = classes.wa.Proposal().fromAttributeValues(
+            id = 0,
+            council = 0,
+            name = '',
+            category = '',
+            author = '',
+            legal = True,
+            quorum = True,
+            coauthors = []
+        )
+        return parsed_xml
